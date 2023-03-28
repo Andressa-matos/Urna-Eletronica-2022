26858ff913f806d4f57c71487627b91d360ef9d5
Andressa  Matos <94369142+Andressa-matos@users.noreply.github.com>
2023-03-28 20:10:54 -0300

    Create etapas.js

---
+++ etapas.js
@@ -0,0 +1,196 @@
+let etapas = [
+    {
+        titulo: 'Deputado Federal',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '5511',
+                nome: 'Célio Stuart',
+                partido: 'Partido Verde',
+                vice: 'Galba Viana',
+                fotos: [
+                    {url: 'https://media.discordapp.net/attachments/1037508964981669952/1037510476348784640/unknown.png', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '1313',
+                nome: 'Luizianne lins',
+                partido: 'PT',
+                vice: '',
+                fotos: [
+                    {url: 'https://images.app.goo.gl/vU4dHNGwjRyVfaSQA', legenda: 'Deputado Federal'}
+                ]
+            },
+
+            {
+                numero: '1023',
+                nome: 'Pastor Abraão',
+                partido: 'Republicanos',
+                fotos: [
+                    {url: 'https://images.app.goo.gl/vU4dHNGwjRyVfaSQA', legenda: 'Deputado Federal'}
+                ]
+            },
+            
+        ]
+    },
+
+
+
+    
+// Deputado Estadual
+{
+    titulo: 'Deputado Estadual',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: '10123',
+            nome: 'Davi Durand',
+            partido: 'Republicanos',
+            vice: 'Galba Viana',
+            fotos: [
+                {url: 'celioS.jpg', legenda: 'Deputado Federal'}
+            ]
+        },
+        {
+            numero: '44888',
+            nome: 'Oscar Rodrigues',
+            partido: 'União Brasil',
+            vice: 'Zé Vital',
+            fotos: [
+                {url: 'https://images.app.goo.gl/vU4dHNGwjRyVfaSQA', legenda: 'Deputado Federal'}
+            ]
+        },
+    
+        {
+            numero: '45123',
+            nome: 'Emília Rodrigues',
+            partido: 'PSDB',
+            vice: 'Silvio Nascimento',
+            fotos: [
+                {url: 'https://images.app.goo.gl/vU4dHNGwjRyVfaSQA', legenda: 'Deputado Federal'}
+            ]
+        },
+        
+    ]
+    },
+    
+
+    // SENADOR  
+    {
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '131',
+                nome: 'Camilo Santana',
+                partido: 'PT',
+                vice: 'Cic',
+                fotos: [
+                    {url: '3.jpg', legenda: 'Prefeito'},
+                    {url: '4.jpg', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },
+
+            
+
+            {
+                numero: '555',
+                nome: 'Erica Amorim',
+                partido: 'PSD',
+                vice: 'Zul',
+                fotos: [
+                    {url: '5.jpg', legenda: 'Prefeito'},
+                    {url: '6.jpg', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            }
+            
+        ]
+    },
+
+// GOVERNADOR
+{
+    titulo: 'GOVERNADOR',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '44',
+            nome: 'Capitão Wagner',
+            partido: 'União Brasil',
+            vice: 'Raimundo Matos',
+            fotos: [
+                {url: 'capitaowg.jpg', legenda: 'Governador'}
+            ]
+        },
+        {
+            numero: '12',
+            nome: 'Roberto Cláudio',
+            partido: 'PDT',
+            vice: 'Domingos Filho',
+            fotos: [
+                {url: 'https://images.app.goo.gl/XfrVKHPU4VbbPKb16', legenda: 'Governador'}
+            ]
+        },
+
+        {
+            numero: '13',
+            nome: 'Elmano',
+            partido: 'PT',
+            vice: 'jade Romero',
+            fotos: [
+                {url: 'https://images.app.goo.gl/vU4dHNGwjRyVfaSQA', legenda: 'Governador'}
+            ]
+        },
+
+    {
+          numero: '160',
+          nome: 'carlos Silva',
+          partido: 'PSTU',
+          vice: 'Zul',
+          fotos: [
+                {url: '5.jpg', legenda: 'Prefeito'},
+                {url: '6.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+    
+    
+
+        
+// PRESIDENTE
+{
+    titulo: 'Presidente',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '13',
+            nome: 'Lula',
+            partido: 'PT',
+            fotos: [
+                {url: 'LULA.jpj', legenda: 'Presidente'}
+            ]
+        },
+
+        {
+            numero: '22',
+            nome: 'Bolsonaro',
+            partido: 'PL',
+            fotos: [
+                {url: 'https://images.app.goo.gl/vU4dHNGwjRyVfaSQA', legenda: 'Presidente'}
+            ]
+        },
+
+        {
+        numero: '15',
+            nome: 'Simone Tebet',
+            partido: 'MDB',
+            fotos: [
+                {url: 'https://images.app.goo.gl/vU4dHNGwjRyVfaSQA', legenda: 'Presidente'}
+            ]
+        }
+        
+      ]
+    }
+]
